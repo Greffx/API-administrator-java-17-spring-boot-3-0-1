9af409a810367b63e7540b3b3854928dcedd6aa0
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-16 14:20:27 -0300

    Person controller & service with more methods/endpoints

--- src/main/java/com/eduardogreff/apiRestProject/controllers/PersonController.java
+++ src/main/java/com/eduardogreff/apiRestProject/controllers/PersonController.java
@@ -1,13 +1,11 @@
 package com.eduardogreff.apiRestProject.controllers;
 
-import com.eduardogreff.apiRestProject.entities.Person;
 import com.eduardogreff.apiRestProject.entities.dto.PersonDTO;
 import com.eduardogreff.apiRestProject.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,5 +19,16 @@ public class PersonController {
     @GetMapping
     public ResponseEntity<List<PersonDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll().stream().map(PersonDTO::new).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(new PersonDTO(service.findById(id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<PersonDTO> create(@RequestBody PersonDTO personDTO) {
+        service.create(personDTO);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(personDTO.getId()).toUri()).build();
     }
 }
--- src/main/java/com/eduardogreff/apiRestProject/services/PersonService.java
+++ src/main/java/com/eduardogreff/apiRestProject/services/PersonService.java
@@ -1,11 +1,14 @@
 package com.eduardogreff.apiRestProject.services;
 
 import com.eduardogreff.apiRestProject.entities.Person;
+import com.eduardogreff.apiRestProject.entities.dto.PersonDTO;
 import com.eduardogreff.apiRestProject.repositories.PersonRepository;
+import com.eduardogreff.apiRestProject.services.exceptions.PersonNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService {
@@ -15,5 +18,14 @@ public class PersonService {
 
     public List<Person> findAll() {
         return repository.findAll();
+    }
+
+    public Person findById(Long id) {
+        Optional<Person> person = repository.findById(id);
+        return person.orElseThrow(() -> new PersonNotFound("This value is invalid, try another one."));
+    }
+
+    public void create(PersonDTO personDTO) {
+        repository.save(new Person(personDTO));
     }
 }
