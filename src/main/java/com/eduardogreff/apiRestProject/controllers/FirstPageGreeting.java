90927fefaa63d4654de85d4c44b7a87d352e9993
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-16 12:39:03 -0300

    First page controller

---
+++ src/main/java/com/eduardogreff/apiRestProject/controllers/FirstPageGreeting.java
@@ -0,0 +1,17 @@
+package com.eduardogreff.apiRestProject.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FirstPageGreeting {
+
+    @GetMapping
+    public ResponseEntity<String> firstPageGreeting(@RequestParam(name = "name", defaultValue = "World") String name) {
+        return ResponseEntity.ok().body(String.format("Hello %s!", name));
+    }
+}
