35d7ed243b07a413cb0b966af9967d936d6e188c
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-16 12:12:24 -0300

    Initial commit

---
+++ src/main/java/com/eduardogreff/apiRestProject/ApiRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eduardogreff.apiRestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardogreff/apiRestProject/ApiRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardogreff.apiRestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
