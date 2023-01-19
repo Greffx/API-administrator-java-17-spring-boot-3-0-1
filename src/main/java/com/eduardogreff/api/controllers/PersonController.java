ccb6ac31cb83d167e62b4c180d134ba1a26c2ce2
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-19 16:32:41 -0300

    Producing another type besides JSON, like XML

--- src/main/java/com/eduardogreff/api/controllers/PersonController.java
+++ src/main/java/com/eduardogreff/api/controllers/PersonController.java
@@ -3,6 +3,7 @@ package com.eduardogreff.api.controllers;
 import com.eduardogreff.domain.entities.dto.PersonDTO;
 import com.eduardogreff.domain.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,24 +17,26 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<List<PersonDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
     }
 
-    @PostMapping
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Void> create(@RequestBody PersonDTO personDTO) {
         service.create(personDTO);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(personDTO.getId()).toUri()).build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Void> put(@RequestBody PersonDTO dto, @PathVariable Long id) {
         service.put(dto, id);
         return ResponseEntity.noContent().build();
