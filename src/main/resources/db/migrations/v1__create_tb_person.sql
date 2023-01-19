56b9baf656cfd4fc0cbef63337d5654f466a3471
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-19 14:22:48 -0300

    Migration MySQL workbench

--- src/main/resources/db/migrations/v1__create_tb_person.sql
+++ src/main/resources/db/migrations/v1__create_tb_person.sql
@@ -1,10 +1,13 @@
-CREATE TABLE IF NOT EXISTS `tb_person` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `age` int DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+--CREATE TABLE IF NOT EXISTS `tb_person` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `age` int DEFAULT NULL,
+--  `city` varchar(255) DEFAULT NULL,
+--  `email` varchar(255) DEFAULT NULL,
+--  `first_name` varchar(255) DEFAULT NULL,
+--  `gender` varchar(255) DEFAULT NULL,
+--  `last_name` varchar(255) DEFAULT NULL,
+--  PRIMARY KEY (`id`)
+--);
+
+
+--test failed probably because of paths or folder name, idk, but i tried to make it work.
--- src/main/resources/db/migrations/v2__populate_tb_person.sql
+++ src/main/resources/db/migrations/v2__populate_tb_person.sql
@@ -1,7 +1,10 @@
-INSERT INTO `tb_person` VALUES
-(1,23,'Porto Alegre','eduardo1@gmail.com','Eduardo','male','Greff'),
-(2,21,'Kansas','rebeca1@gmail.com','Rebeca','female','Silva'),
-(3,32,'Berlin','jonas1@gmail.com','Jonas','male','Grafeno'),
-(4,12,'France','pedro1@gmail.com','Pedro','male','Pedroso'),
-(5,53,'Porto','gabriel1@gmail.com','Gabriel','male','Ferraz'),
-(6,33,'Canoas','marcela1@gmail.com','Marcela','female','Junior');
+--INSERT INTO `tb_person` VALUES
+--(1,23,'Porto Alegre','eduardo1@gmail.com','Eduardo','male','Greff'),
+--(2,21,'Kansas','rebeca1@gmail.com','Rebeca','female','Silva'),
+--(3,32,'Berlin','jonas1@gmail.com','Jonas','male','Grafeno'),
+--(4,12,'France','pedro1@gmail.com','Pedro','male','Pedroso'),
+--(5,53,'Porto','gabriel1@gmail.com','Gabriel','male','Ferraz'),
+--(6,33,'Canoas','marcela1@gmail.com','Marcela','female','Junior');
+
+
+--test failed probably because of paths or folder name, idk, but i tried to make it work.
