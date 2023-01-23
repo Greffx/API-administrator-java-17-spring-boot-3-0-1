e313075b0f9573639c9e34029edfb7caeebea66f
Eduardo Greff <eduardogreff0@gmail.com>
2023-01-23 17:56:27 -0300

    Customazing controllers with swagger

--- src/main/java/com/eduardogreff/api/controllers/FirstPageGreeting.java
+++ src/main/java/com/eduardogreff/api/controllers/FirstPageGreeting.java
@@ -1,5 +1,8 @@
 package com.eduardogreff.api.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
-@Tag(name = "Greeting Page", description = "First Page, you can write your name in URL path and be greeted")
+@RequestMapping("/greeting")
+@Tag(name = "Greeting", description = "Endpoint to greet people.")
 public class FirstPageGreeting {
 
     @GetMapping
+    @Operation(summary = "Greets a person", description = "Says hello to people.",
+            responses = {
+                    @ApiResponse(description = "Success case", responseCode = "200"),
+                    @ApiResponse(description = "Not Found case", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity<String> firstPageGreeting(@RequestParam(name = "name", defaultValue = "World") String name) {
         return ResponseEntity.ok().body(String.format("Hello %s!", name));
     }
--- src/main/java/com/eduardogreff/api/controllers/PersonController.java
+++ src/main/java/com/eduardogreff/api/controllers/PersonController.java
@@ -2,6 +2,11 @@ package com.eduardogreff.api.controllers;
 
 import com.eduardogreff.domain.entities.dto.PersonDTO;
 import com.eduardogreff.domain.services.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -20,17 +25,43 @@ public class PersonController {
     private PersonService service;
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Operation(summary = "Finds a list of people",description = "Finds a list of people.", tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success case", responseCode = "200", content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))
+                    }),
+                    @ApiResponse(description = "Not Found case", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity<List<PersonDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
 
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Operation(summary = "Finds person", description = "Finds a person by his id in URL path.",
+            responses = {
+                    @ApiResponse(description = "Success case", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                    @ApiResponse(description = "Not Found case", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request case", responseCode = "400", content = @Content)
+            }
+    )
     public ResponseEntity<PersonDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
     }
 
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Operation(summary = "creates a person", description = "Creates a new person by passing data in JSON or XML.",
+            responses = {
+                    @ApiResponse(description = "Created case", responseCode = "201",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                    @ApiResponse(description = "Bad Request case", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found case", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity<Void> create(@RequestBody PersonDTO personDTO) {
         service.create(personDTO);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -39,12 +70,27 @@ public class PersonController {
 
     @PutMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Operation(summary = "Update a person",description = "Upgrades information of a person by passing data in JSON or XML.",
+            responses = {
+                    @ApiResponse(description = "No content case", responseCode = "204",
+                            content = @Content),
+                    @ApiResponse(description = "Bad Request case", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found case", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity<Void> put(@RequestBody PersonDTO dto, @PathVariable Long id) {
         service.put(dto, id);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a person", description = "Deletes a person by your id.",
+            responses = {
+                    @ApiResponse(description = "No content case", responseCode = "204",  content = @Content),
+                    @ApiResponse(description = "Bad Request case", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found case", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
